767c6476a2d675c923e3c784b05b7bea94f449f2
Ahmad Yoosofan <yoosofan@gmx.com>
2020-11-21 10:23:53 +0330

    sdf

---
+++ myArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class myArray{
+  static const int MAX = 100;
+  double a[MAX];
+  int n;
+  public:
+  double get(const int index = 0 ){
+    if(index < n) return a[index];
+    cout << "Error in getting an element of myArray" << endl;
+    return 0;
+  }
+  void set(const int index, const double value){
+    if( index < n ) a[index] = value;
+    cout << "Error accessing myArray " << endl;
+  }
+  myArray(const double *ma = nullptr, int k = 0){
+    if( k > MAX ) 
+      k = MAX ;
+    for(n = k--; k >= 0; k--)
+      a[k] = ma[k];
+  }
+  void print(void){
+    cout << " n = " << n << endl;
+    for(int i = 0; i < n; i++)
+      cout << "a[" << i << "] = " << a[i] << endl;
+  }
+};
+void f1(void);
+int main(){
+  f1();
+  return 0;
+}
+void f1(void){
+  double x[]{10, 12, 34, 54};
+  myArray d(x, sizeof(x) / sizeof(double));
+  d.set(30, 500);
+  // d.n = 500; // So dangerous
+  d.print();
+}
