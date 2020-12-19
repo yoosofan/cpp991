66d809636d732ca9d576bdc702c45bd1530535d4
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-19 10:00:35 +0330

    date: Sat 19 Dec 2020 10:00:35 AM +0330

--- inheritance/inheritance170.cpp
+++ inheritance/inheritance170.cpp
@@ -6,23 +6,33 @@ protected:
   string name;
   string color;
 public:
-  int Area(void){cout<<"in Shape"<<endl;return 0;}
+  int Area(void){
+    cout << __LINE__ << "\t: " << "in Shape" << endl;
+    return 0;
+  }
 };
 class Square: public Shape{
   int d;
 public:
-  int Area(void){cout<<"in Square"<<endl;return d*d;}
+  int Area(void){
+    cout << __LINE__ << "\t: " << "in Square" << endl;
+    return d*d;
+  }
   Square(int d=1){this->d=d;}
 };
-void f1(Shape a){cout<<a.Area()<<endl;}
+void f1(Shape a){
+  cout << __LINE__ << "\t: " << a.Area() << endl;
+}
 int main(){
   Shape s;
   f1(s);
   Square sq(3);
   f1(sq);
-  //cout<<sq.Area()<<endl;
-  Shape *ps=&s; cout<<ps->Area()<<endl;
-  ps=&sq; cout<<ps->Area()<<endl;
+  cout << __LINE__ << "\t: " << sq.Area() << endl;
+  Shape *ps=&s; 
+  cout << __LINE__ << "\t: " << ps->Area() << endl;
+  ps = &sq; 
+  cout << __LINE__ << "\t: " << ps->Area() << endl;
   A *pa;
   return 0;
 }
