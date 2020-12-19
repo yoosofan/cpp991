dacb71b2e358747b07e1096a868867606e7c66bb
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-19 10:38:40 +0330

    date: Sat 19 Dec 2020 10:38:40 AM +0330

--- inheritance/inheritance190.cpp
+++ inheritance/inheritance190.cpp
@@ -1,27 +1,45 @@
 // Error
 #include<iostream>
 using namespace std;
+
 class A{};
+
 class Shape: public A{
 protected:
   string name;
   string color;
 public:
-  int Area(void){cout<<"in Shape"<<endl;return 0;}
-  void show(void){cout<<"show in shape"<<endl;}
+  int Area(void){
+    cout<< __LINE__ << "\t: " << "in Shape"<<endl;
+    return 0;
+  }
+  void show(void){
+    cout << __LINE__ << "\t: " << "show in shape" << endl;
+  }
 };
+
 class Square: protected Shape{
   int d;
 public:
-  int Area(void){cout<<"in Square"<<endl;return d*d;}
+  int Area(void){
+    cout << __LINE__ << "\t: " << "in Square" << endl;
+    return d*d;
+  }
   Square(int d=1){this->d=d;}
 };
-void f1(Shape a){cout<<a.Area()<<endl;a.show();}
+
+void f1(Shape a){
+  cout << __LINE__ << "\t: " << a.Area() << endl;
+  a.show();
+}
 int main(){
-  Shape s;  f1(s);
-  Square sq(3);  f1(sq);
+  Shape s;  
+  f1(s);
+  Square sq(3);  
+  f1(sq);
   //cout<<sq.Area()<<endl;
-  Shape *ps=&s; cout<<ps->Area()<<endl;
+  Shape *ps = &s; 
+  cout << __LINE__ << "\t: " << ps->Area() << endl;
   //ps=&sq; cout<<ps->Area()<<endl;
   A *pa;
   return 0;
