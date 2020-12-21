b0c92f616be66dc3d2f8f0c347d5c377abe0a126
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-21 09:57:45 +0330

    date: Mon 21 Dec 2020 09:57:45 AM +0330

--- inheritance/inheritance260_output.cpp
+++ inheritance/inheritance260_output.cpp
@@ -1,20 +1,17 @@
 #include<iostream>
 using namespace std;
 class Shape{
- protected:
+  protected:
   string name;
   string color;
- public:
+public:
   Shape(string name="shape", string color="white"):name(name),color(color){}
-  virtual int Area(void){return 0;}
+  virtual int Area(void) = 0;
+  // virtual static void f(void){cout<<"Shape virtual static f"<<endl;}
 };
 class Square: public Shape{
   int d;
 public:
-  void Show(void){
-    cout << "in Square Area of Shape: " << Shape::Area() << endl;
-    cout << "in Square Area of Square: " << Area() << endl;
-  }
   virtual int Area(void){return d*d;}
   Square(int d=1, string name="square", 
     string color="blue"):Shape(name, color){this->d=d;}
@@ -22,17 +19,15 @@ public:
       cout<<"name: "<<this->name<<'\t'<<"color: "<<
       this->color<<'\t'<<"d: "<<this->d<<endl;
   }
+  // virtual static void f(void){cout<<"Square virtual static f"<<endl;}
 };
 int main(){
-  Shape s;
-  cout << s.Area() << endl;
-  Square sq(3); 
-  sq.Show();
+  // Shape s;  cout<<s.Area()<<endl;
+  Square sq(3);
   cout << sq.Area() << endl;
-  Shape *ps = &s; 
-  cout << ps->Area() << endl;
-  ps = &sq; 
-  cout << ps->Area() << endl;
+  //Shape *ps = &s;   cout << ps->Area() << endl;
+  Shape * ps = & sq; 
+  cout << ps -> Area() << endl;
   sq.printAll();
   return 0;
 }
