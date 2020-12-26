9ae495e8fe0337b70874306cd24c6450e10bce6b
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-26 09:53:17 +0330

    date: Sat 26 Dec 2020 09:53:17 AM +0330

--- template/22_swap_before_template.cpp
+++ template/22_swap_before_template.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 void swap(double&, double&);
 void swap(string&, string&);
+struct complexCls{
+   double re=0,im=0;
+   //explicit complexCls(double m=0 , double n=0) {r=m;i=n;}
+   complexCls(double m=0 , double n=0) {r=m;i=n;}
+   //complexCls(int m=0 , int n=0) {r=m;i=n;}
+   void Show(){cout<<'('<<r<<','<<i<<')'<<'\t'<<endl;}
+   double Magnitude(){return sqrt(r*r+i*i);}
+   //operator bool() const {cout<<"aaa:::: "<<endl;return r==0 && i==0 ? false : true;}
+   bool operator!() const {cout<<"aaa:::: "<<endl;return !(r==0 && i==0 ? false : true);}
+   complexCls operator++(void){   // ++a
+    cout<<"not int";    i++; r++;    return *this;  }
+   complexCls&& operator++(int dummy){ // a++
+    cout<<"int";    complexCls result=*this;    r++;    i++;    return result;  }
+}; 
+complexCls operator +(const complexCls& a, const complexCls& b){
+  complexCls result ;
+  result.r = a.r + b.r;
+  result.i = a.i + b.i;
+  return result;
+}
+complexCls operator -(const complexCls& a,const complexCls& b){
+  complexCls result ;
+  result.r = a.r - b.r;
+  result.i = a.i - b.i;
+  return result;
+}
+complexCls operator *(const complexCls& a, const complexCls& b){
+  complexCls result;
+  result.r = a.r * b.r;
+  result.i = a.i * b.i;
+  return result;
+}
 int main(){
   double a = 2, b = 4 ;
   cout << "a: " << a << "\tb: " << b << endl;
