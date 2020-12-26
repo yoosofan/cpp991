070366c06b0f0ca39ebbe35f0c9d04b1839e8175
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-26 09:45:38 +0330

    date: Sat 26 Dec 2020 09:45:38 AM +0330

--- template/22_swap_before_template.cpp
+++ template/22_swap_before_template.cpp
@@ -4,9 +4,9 @@ void swap(double&, double&);
 void swap(string&, string&);
 int main(){
   double a = 2, b = 4 ;
-  cout << "a: " << a << "b: " << b << endl;
+  cout << "a: " << a << "\tb: " << b << endl;
   swap(a, b);
-  cout << "a: " << a << "b: " << b << endl;
+  cout << "a: " << a << "\tb: " << b << endl;
   string sta = "Ali" , stb = "Ahmad";
   cout << "sta: " << sta << "\tstb: " << stb << endl;
   swap(sta, stb);
