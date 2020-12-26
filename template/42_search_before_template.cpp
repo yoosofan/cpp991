e37e9eec2bfb63c8b41dff86ed8d391f67313fc0
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-26 09:48:07 +0330

    date: Sat 26 Dec 2020 09:48:07 AM +0330

--- template/42_search_before_template.cpp
+++ template/42_search_before_template.cpp
@@ -1,22 +1,17 @@
-#include<iostrea>
+#include<iostream>
 using namespace std;
-void swap(double&, double&);
-void swap(string&, string&);
+template<typename T> int search(T *a, T v, int n);
 int main(){
-  double ae[20] = {3, 4, 5, 6, 8, 9};
-  cout << search(ae, 3, 6) << endl;
-  cout << search(ae, 33, 6)  << endl;
-  string am[]={"ali", "reza", "kamran", "hamid"};
-  cout << search(am,"reza",4) << endl;
+  double ae[20]={3, 4, 5, 6, 8, 9};
+  cout << search<double>(ae, 3, 6) << endl;
+  cout << search<double>(ae, 33, 6) << endl;
+  string am[] = {"ali", "reza", "kamran", "hamid"};
+  cout << search<string>(am, "reza", 4) << endl;
   return 0;
 }
-void swap(double& a, double& b){
-  double temp = a;
-  a = b;
-  b = temp;
-}
-void swap(string&, string&){
-  string temp = a;
-  a = b;
-  b = temp;
+template<typename T> int search(T *a, T v, int n){
+  for(int i = 0; i < n; i++)
+    if(a[i] == v)
+      return i;
+  return -1;
 }
