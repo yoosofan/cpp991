1d13aeb67abfce22b1706a1f16a07cfe9db546ed
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-26 09:50:33 +0330

    date: Sat 26 Dec 2020 09:50:33 AM +0330

--- template/42_search_before_template.cpp
+++ template/42_search_before_template.cpp
@@ -1,15 +1,22 @@
 #include<iostream>
 using namespace std;
-template<typename T> int search(T *a, T v, int n);
+int search(double *a, double v, int n);
+int search(string *a, string v, int n);
 int main(){
   double ae[20]={3, 4, 5, 6, 8, 9};
-  cout << search<double>(ae, 3, 6) << endl;
-  cout << search<double>(ae, 33, 6) << endl;
+  cout << search(ae, 3, 6) << endl;
+  cout << search(ae, 33, 6) << endl;
   string am[] = {"ali", "reza", "kamran", "hamid"};
-  cout << search<string>(am, "reza", 4) << endl;
+  cout << search(am, "reza", 4) << endl;
   return 0;
 }
-template<typename T> int search(T *a, T v, int n){
+int search(double *a, double v, int n){
+  for(int i = 0; i < n; i++)
+    if(a[i] == v)
+      return i;
+  return -1;
+}
+int search(string *a, string v, int n){
   for(int i = 0; i < n; i++)
     if(a[i] == v)
       return i;
