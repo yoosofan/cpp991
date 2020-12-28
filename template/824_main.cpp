98f27027886baf63c9b7e9b5f52998df6e5bee3f
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-28 11:28:33 +0330

    date: Mon 28 Dec 2020 11:28:33 AM +0330

---
+++ template/824_main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include "840_complexcls.h"
+#include "824_myArray.h"
+using namespace std;
+void f1(void);
+template<typename T>void f2(myArray<T>);
+int main(){
+  f1();
+  return 0;
+}
+void f1(void){
+  double x[]{10, 12, 34, 54};
+  myArray<double> d(x, sizeof(x) / sizeof(double)), d2;
+  d2 = d;
+  cout << d;
+  cout << endl;
+  if(d2 == d) 
+    cout << "d2 == d" << endl
+  myArray<double> b;
+  cin >> b ;
+  cout << b[1] << endl;
+  b[1] = 15;
+  f2(b);
+  string sx[]{"Kamran", "Reza", "Kmabiz", "Ali"};
+  myArray<string> r(sx, 4), r2 = r;
+  cout << r;
+  cout << endl;
+  if(r2 == r)
+    cout << "r2 == r" << endl;
+  myArray<string> z;
+  cin >> z ;
+  cout << z[1] << endl;
+  z[1] = 15;
+  f2(z);
+  complexCls acmp[] = { {3,5}, {2,4}, {5,9} };
+  myArray<complexCls> myacmp(acmp, 3), myacmp2 = myacmp;
+  cout << myacmp << endl;
+  if( myacmp == myacmp2)
+    cout << "myacmp == myacmp2" << endl;
+}
+template<typename T> void f2(myArray<T> k){
+  k.print();
+}
---
+++ template/824_myArray.h
@@ -0,0 +1,96 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include "840_complexcls.h"
+#include "840_complexcls.h"
+using namespace std;
+
+template<typename T> class myArray{
+  static const int MAX = 100;
+  T a[MAX];
+  int n;
+  void copy(const  myArray& b){
+    this -> n = b.n;
+    for(int i = 0; i < n; i++)
+    this -> a[i] = b.a[i];
+  }
+  public:
+  // myArray() = delete ;
+  T get(const int index = 0 ) const{
+    if(index >= n) {
+      cout << "Error in getting an element of myArray" << endl;
+      exit(0);
+    }
+    return a[index];
+  }
+  void set(const int index, const T value){
+    if( index < n ) a[index] = value;
+    cout << "Error accessing myArray " << endl;
+  }
+  myArray(const T *a = nullptr, int n = 0){
+    if( n > MAX )
+      n = MAX ;
+    for(this -> n = n--; n >= 0; n--)
+      this -> a[n] = a[n];
+  }
+  explicit myArray(T x){
+    n = 1;
+    a[0] = x;
+    cout << "construcotr myArray: " << a[0] << endl;
+  }
+  myArray(const myArray& b){
+    cout << "copy construcotr" << endl;
+    copy(b);
+  }
+  ~myArray(){
+    cout << "Destructor" << endl;
+  }
+  void print(void) const{
+    cout << " n = " << n << endl;
+    for(int i = 0; i < n; i++)
+      cout << "a[" << i << "] = " << a[i] << endl;
+  }
+  myArray operator=(const myArray& b){
+    cout << "Assignment myArray" << endl;
+    copy(b);
+    return *this;
+  }
+  T& operator[](int index){
+    if(index >= n){
+      cout << "index out of range: " << index << endl;
+      index = 0;
+      exit(0);
+    }
+    return a[index];
+  }
+
+  // https://en.cppreference.com/w/cpp/language/friend
+  friend bool operator==(const myArray& a, const myArray& b);
+
+  friend istream& operator>> (istream& in1, myArray& b){
+    // delete[] b.a;  b.a = nullptr;
+    b.n = 0;
+    cout << "Enter n ";
+    in1 >> b.n;
+    while( b.n < 0 || b.n > myArray::MAX ){
+      cout << "number of element must be greater than 0 and less than  "
+        << myArray::MAX << endl;
+      in1 >> b.n;
+    }
+    // b.a = new double[b.n];
+    if( b.n )
+      for(int i = 0; i < b.n; i++){
+        cout << "Enter a["<< i << "]: ";
+        in1 >> b.a[i];
+      }
+    // else b.a = nullptr;
+    return in1;
+  }
+  friend ostream& operator<< (ostream& out1, const myArray& b){
+    out1 << "n = " << b.n << endl;
+    for(int i = 0; i < b.n ; i++)
+      out1 << "a[" << i << "]= " << b.a[i] << endl;
+    return out1;
+  }
+
+};
