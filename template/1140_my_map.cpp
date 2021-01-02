90d87574933e8ee63f7a45e86e1becb581485b21
Ahmad Yoosofan <yoosofan@gmx.com>
2021-01-02 09:49:49 +0330

    date: Sat 02 Jan 2021 09:49:49 AM +0330

--- template/1140_my_map.cpp
+++ template/1140_my_map.cpp
@@ -1,22 +1,28 @@
 #include<iostream>
 using namespace std;
+
 template<typename T=double, typename I=string, int MAX=300>
 class myArray{
   T a[MAX];
   int n;
   I s[MAX];
+  
   public:
+  
   myArray(){n=0;}
+  
   T& operator[](string m1){
     int i;
     for(i=0; i<n; i++)
       if(s[i] == m1 )
         return a[i];
-    if(n>=MAX) throw "not suffecient memory";
+    if(n>=MAX) 
+      throw "not suffecient memory";
     s[n]=m1;
     return a[n++];
   }
 };
+
 int main(){
   myArray<> m1;
   m1["ali"]=23;
