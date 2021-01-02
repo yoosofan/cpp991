22ff57376f8843a5f35cd8d8f3535142493142a0
Ahmad Yoosofan <yoosofan@gmx.com>
2021-01-02 10:52:52 +0330

    date: Sat 02 Jan 2021 10:52:52 AM +0330

--- exception/220.cpp
+++ exception/220.cpp
@@ -3,31 +3,69 @@
 #include <string>
 #include <cstring>
 #include <exception>
+using namespace std;
 
 class myException {
   public:
   int errorNumber;
   char nameError[50];
   public:
-  myException(int errn=0 , const char *errc="Unknown error "){
+  myException(int errn=0, const char *errc="Unknown error "){
     errorNumber = errn;
     strcpy(nameError , errc);
   }
 };
-using namespace std;
+
+void f1(int i);
+void f2(int mm);
+void f4(int mm);
+void f3(void);
+
+struct RootCls{ double root1 , root2;};
+RootCls solve(double a, double b, double c);
+
+int main(){
+  try{
+    f1(1);
+    f2(12);
+    f3();
+  } catch(int m) {
+    cout <<"catch expception "<<m<<endl;
+  } catch (char * sm){
+    cout<<"catch f4 error in main "<< sm<<endl;
+  }
+  try{
+    f2(13);
+    RootCls rc1 =solve(4 , 5, 2);
+    cout<< rc1.root1<<" root2  "<<rc1.root2<< endl;
+  } catch(int nn){
+    cout <<"Second catch in main "<<nn << endl;
+  } catch(myException a1){
+    cout<<"My Exception catch "<< a1.errorNumber <<"  "<<a1.nameError<<endl;
+  }
+  try{
+    throw 5;
+    cout <<"Execution will not reach to this statement"<<endl;
+  } catch(int  ll){ cout <<"Third catch in main "<<endl;}
+  cout << "End "<<endl;
+  return 0;
+}
 
 void f1(int i){
   if( i <= 0)   throw 1;
   else    cout << i<<endl;
 }
+
 void f2(int mm){
   if (mm > 20)    throw 2;
   else    cout<<mm<<endl;
 }
+
 void f4(int mm){
   if(mm<10) throw "Error ";
   cout << "In f4 mm "<< mm<<endl;
 }
+
 void f3(void){
   try {
     f1(-3);
@@ -36,7 +74,7 @@ void f3(void){
     cout <<"In f3 "<<mm<<endl;
   }
 }
-struct RootCls{ double root1 , root2;};
+
 RootCls solve(double a, double b, double c){
   RootCls retVal ; double delta;
   if(a == 0 && b == 0) throw myException(7,"a == 0 and b == 0 ");
@@ -49,33 +87,4 @@ RootCls solve(double a, double b, double c){
     retVal.root2 = (-b - delta)/(2*a);
   }
   return retVal;
-}
-
-int main(){
-  try{
-    f1(1);
-    f2(12);
-    f3();
-
-  }catch(int m){
-    cout <<"catch expception "<<m<<endl;
-  }catch (char * sm){
-    cout<<"catch f4 error in main "<< sm<<endl;
-  }
-  try{
-    f2(13);
-    RootCls rc1 =solve(4 , 5, 2);
-    cout<< rc1.root1<<" root2  "<<rc1.root2<< endl;
-  }catch(int nn){
-    cout <<"Second catch in main "<<nn << endl;
-  }
-  catch(myException a1){
-    cout<<"My Exception catch "<< a1.errorNumber <<"  "<<a1.nameError<<endl;
-  }
-  try{
-    throw 5;
-    cout <<"Execution will not reach to this statement"<<endl;
-  }catch(int  ll){ cout <<"Third catch in main "<<endl;}
-  cout<< "End "<<endl;
-  return 0;
 }
