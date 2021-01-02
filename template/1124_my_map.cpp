717db65bd5e830566a13366b14e282e042bff301
Ahmad Yoosofan <yoosofan@gmx.com>
2021-01-02 09:43:45 +0330

    date: Sat 02 Jan 2021 09:43:45 AM +0330

---
+++ template/1124_my_map.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+template<typename T>
+class myArray{
+  static int MAX = 300;
+  T a[MAX];
+  int n;
+  string s[MAX];
+  public:
+  myArray(){n=0;}
+  T& operator[](string m1){
+    int i;
+    for(i=0; i<n; i++)
+      if(s[i] == m1 )
+        return a[i];
+    if(n < MAX){
+      s[n] = m1;
+      return a[n++];
+    }
+    return a[n-1];
+  }
+};
+int main(){
+  myArray<double> m1;
+  m1["ali"] = 23;
+  m1["reza"] = 45;
+  cout << m1["ali"] << endl;
+  return 0;
+}
